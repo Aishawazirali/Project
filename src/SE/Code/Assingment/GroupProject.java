7aa6f429e4bc35bfa7de3e496ab21e4a72c0e7f3
D E L L <D E L L@192.168.0.111>
2023-05-22 10:49:56 +0500

    first commit.....

---
+++ src/SE/Code/Assingment/GroupProject.java
@@ -0,0 +1,18 @@
+package SE.Code.Assingment;
+
+public class GroupProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 6;
+		if(age>20) {
+			System.out.println("Yes You can drive!");
+		}
+		else {
+			System.out.println("No You can not drive");
+		}
+
+
+	}
+
+}
---
+++ src/aishaandunsa/groupassignment.java
@@ -0,0 +1,16 @@
+package aishaandunsa;
+
+public class groupassignment {
+
+	public static void main(String[] args) {
+		int age = 6;
+		if(age>20) {
+			System.out.println("Yes You can drive!");
+		}
+		else {
+			System.out.println("No You can not drive");
+		}
+
+	}
+
+}
