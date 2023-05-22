8879d03db11d7a605daa167247ec2310f30cc073
HP <HP@LAPTOP-S7J6VOOR>
2023-05-22 21:53:52 +0500

    the code is completed...

---
+++ src/SE/Code/Assingment/GroupProject2.java
@@ -0,0 +1,18 @@
+package SE.Code.Assingment;
+
+public class GroupProject2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 6;
+		if(age>20) {
+			System.out.println("Yes You can drive!");
+		}
+		else {
+			System.out.println("No You can not drive");
+		}
+
+
+	}
+
+}
